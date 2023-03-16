114adec05428f2bd6bc241edb794fe383c141740
kmike-wachira <mwkariuki2e@gmail.com>
2023-03-16 18:47:59 +0000

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+/*
+ *main function
+ *
+ * Returns 0
+ */
 int main(void)
 {
 	char str[100];
 
-	strcpy(str, "\"Programming is like building a multilingual puzzle\n");
+	strcpy(str, "\"Programming is like building a multilingual puzzle");
 	puts(str);
 	return (0);
 }
