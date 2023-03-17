3ccf12702ceab6d4fded2194352d14ffa85e3da3
kmike-wachira <mwkariuki2e@gmail.com>
2023-03-17 20:31:46 +0000

    print inalphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *
+ *Return: always 0 (Success)
+ */
+int main(void)
+{
+	char myletter;
+
+	for (myletter = 'a'; myletter <= 'z'; myletter++)
+		putchar(myletter);
+	putchar('\n');
+	return (0);
+}
