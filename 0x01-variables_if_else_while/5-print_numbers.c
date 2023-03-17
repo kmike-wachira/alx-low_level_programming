1687de0f2c34717d645d1fcb7de4ef1381f9694f
kmike-wachira <mwkariuki2e@gmail.com>
2023-03-17 20:53:43 +0000

    print all number to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Entry point
+ *
+ *Return: always 0 (Success)
+ */
+int main(void)
+{
+	int no;
+
+	for (no = 0; no < 10 ; no++)
+	{
+		printf("%d", no);
+	}
+	printf("\n");
+	return (0);
+}
