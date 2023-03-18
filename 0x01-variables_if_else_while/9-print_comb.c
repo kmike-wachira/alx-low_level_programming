c59af3842ccf972b520ef21aaae61bb7f1e3cbaf
kmike-wachira <mwkariuki2e@gmail.com>
2023-03-18 04:28:09 +0000

    all single combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *
+ *Return: always 0 (Success)
+ */
+int main(void)
+{
+	int no;
+
+	for (no = 0; no < 10 ; no++)
+	{
+		putchar((no % 10) + '0');
+		putchar(',');
+		putchar(' ');
+
+	}
+	putchar('\n');
+	return (0);
+}
