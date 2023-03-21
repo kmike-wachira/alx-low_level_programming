9cfa297dd2543a549303fb6ba35ccb1d205ddfd1
kmike-wachira <mwkariuki2e@gmail.com>
2023-03-21 14:07:10 +0000

    Removed main

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,14 @@
 void print_alphabet_x10(void)
 {
 	char i;
+	int j;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (j = 0; j < 10; j++)
 	{
-		_putchar(i);
-	}
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
 	_putchar('\n');
+	}
 }
