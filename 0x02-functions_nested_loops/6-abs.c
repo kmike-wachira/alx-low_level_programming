50f50fb6147138e15a4451b4d1363162023247c2
kmike-wachira <mwkariuki2e@gmail.com>
2023-03-21 18:19:46 +0000

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - function to return the absoluten.
+ *
+ * @n : The character to be checked.
+ *
+ * Return: Return the absolute value
+ */
+int _abs(int n)
+{
+
+	if (n >= 0)
+	{
+		return(n);
+	}
+	else
+		return (-n);
+	
+}
