00d72f7ed255d08f227a2eda791995c4083dea9e
kmike-wachira <mwkariuki2e@gmail.com>
2023-03-21 19:45:35 +0000

    add two number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - function to return the absoluten.
+ *
+ * @digit1 : The first number.
+ * @digit2 : The second number
+ * Return: Return sum
+ */
+int add(int digit1, int digit2)
+{
+
+	return (digit1 + digit2);
+}
