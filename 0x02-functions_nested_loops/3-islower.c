e3506fb42cff0821daadc0d12f9997b9e5f57a7c
kmike-wachira <mwkariuki2e@gmail.com>
2023-03-21 14:21:27 +0000

    is slower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,24 +1,5 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
-/*declare function print alphabet*/
-
-int main(void)
-{
-	int result;
-
-	result = _islower('B');
-
-	_putchar(result + '0');
-	_putchar('\n');
-	return (0);
-}
-
-/**
  * _islower - function to print alphabets
  *
  * Description : take any input return 1 or 0
@@ -30,7 +11,7 @@ int main(void)
 int _islower(int c)
 {
 
-	if (c >= 'a' || c <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
