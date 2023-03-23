83f22a755f811594a147b23fda1b97b4802b10c9
kmike-wachira <mwkariuki2e@gmail.com>
2023-03-23 18:12:06 +0000

    Check whether uppercase or lowercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isupper - check whether character is upper
+ */
+
+int _isupper(char c)
+{
+	if ( c >= 'A' || c <= 'Z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+loop second assignment
