672c090cf65029096f4803d242ab90b84a2964ba
kmike-wachira <mwkariuki2e@gmail.com>
2023-03-23 18:39:24 +0000

    print the numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - check whether character is upper
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
