e60ff4d896861e770b62c8e36abe66450bfd57de
kmike-wachira <mwkariuki2e@gmail.com>
2023-03-23 19:31:11 +0000

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - check whether character is upper
+ *
+ * @n : the numebr of times _wil be printed
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		_putchar('_');
+
+	_putchar('\n');
+
+}
