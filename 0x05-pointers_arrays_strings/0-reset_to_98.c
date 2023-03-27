f406be0cbe6f9b784b121319b772608626408482
kmike-wachira <mwkariuki2e@gmail.com>
2023-03-27 18:25:14 +0000

    pointer to take the variable to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include <main.h>
 /**
  * reset_to_98 - function to reset to 98
- * @n:arameter to get the pointer value
+ * @n : parameter to get the pointer value
  */
 
 void reset_to_98(int *n)
