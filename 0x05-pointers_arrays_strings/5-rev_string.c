5eb251535c2c36ed590ae8a9b5b19f6489924e0b
kmike-wachira <mwkariuki2e@gmail.com>
2023-03-27 19:18:29 +0000

    reverse with caution

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * rev_string - reverse string
+ * @s : string parameter to reverse
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+
+	l = i;
+	i = 0;
+	j = length - 1;
+
+	while (i < j)
+	{
+		k = s[i];
+		s[i] = s[j];
+		s[j] = k;
+
+		i++;
+		j--;
+	}
+}
