6faa5e11634fde795571bc84175cd5c9704502da
kmike-wachira <mwkariuki2e@gmail.com>
2023-03-27 20:34:33 +0000

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,16 +5,14 @@
  */
 void print_rev(char *s)
 {
-	int i;
+	int i = 0;
 
-	while (s[i] != '\0')
+	while (s[i])
 	{
 		i++;
 	}
-	i--;
-	while (s[i] != '\0')
-	{
-		_putchar(s[i--]);
-	}
+	while (--i >= 0)
+		_putchar(s[i]);
 	_putchar('\n');
 }
+
